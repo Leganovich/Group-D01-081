66e947df973aa7e828393be5f41da6805eabd744
Stanislav Leganovich <leganovich@gmail.com>
2024-09-09 21:33:40 +0300

    Add files via upload
    Вебинар №1
    Введение в язык Си
    Задание №3

---
+++ Tree.c
@@ -0,0 +1,14 @@
+ //Урок 1 введение в си, задание 3, елочка
+
+#include <stdio.h>
+
+int main (void)
+{
+  printf ("   *\n");
+  printf ("  ***\n"); 
+  printf (" *****\n");
+  printf ("*******\n");
+  printf (" HHOHH\n");
+  printf (" ZZZZZ\n");
+  return 0; //выход из функции
+}
