eb7eff46e0286bd7a7c1ad0fd4dd929f5ca8919e
Stanislav Leganovich <leganovich@gmail.com>
2024-09-13 11:26:23 +0300

    Create Table_Task4_1.c

---
+++ HomeWork_2/Table_Task4_1.c
@@ -0,0 +1,17 @@
+/*Таблица истинности*/
+#include <stdio.h>
+int main() {
+    printf("A->B =!A||B\n");
+    printf("\n");
+    printf("| A | B |A->B |\n");
+    printf("| 0 | 0 |  1  |\n");
+    printf("| 0 | 1 |  1  |\n");
+    printf("| 1 | 0 |  0  |\n");
+    printf("| 1 | 1 |  1  |\n");
+    printf("=\n");
+    printf("| A | B |!A |!A||B|\n");
+    printf("| 0 | 0 | 1 |  1  |\n");
+    printf("| 0 | 1 | 1 |  1  |\n");
+    printf("| 1 | 0 | 0 |  0  |\n");
+    printf("| 1 | 1 | 0 |  1  |\n");
+}
