0229d06961cc5a0ff20e138d2608b5c100f372c6
Stanislav Leganovich <leganovich@gmail.com>
2024-09-13 14:04:58 +0300

    Update Task_1.c

--- HomeWork_3/Task_1.c
+++ HomeWork_3/Task_1.c
@@ -1,7 +1,7 @@
 /* 
 *  Lecture_3
 *  Task_1
-*  Task: Enter three numbers, their sum and product
+*  Task: Ввести три целых числа и найти их сумму
 */
 
 #include <stdio.h>
@@ -9,9 +9,11 @@ int main() {
     int a, b, c; // переменные для вводимых чисел
     int sum; //переменная для расчетов
     printf("Введите через пробел три целых числа\n");
-    scanf("%d%d%d", &a, &b, &c); // функция считывания данных, в фунцию передается адрес переменных через &
+    scanf("%d%d%d", &a, &b, &c); // функция считывания данных, в фунцию передается адрес переменных через & (указатель)
     printf("%d %d %d\n", a, b, c); 
     sum = a+b+c; 
     printf("%d+%d+%d=%d", a, b, c, sum); 
+    getchar();
+    getchar();
     return 0;
 }
